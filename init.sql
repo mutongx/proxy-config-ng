e753a25bb62ada63fd84919c1dc5670ef5063c8c
Mut Ze <mutongsoft@gmail.com>
2023-05-25 18:20:07 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,12 @@
-CREATE TABLE host (name TEXT, addr4 TEXT, addr6 TEXT);
-CREATE TABLE user (name TEXT, token TEXT, config JSON);
-CREATE TABLE access (user TEXT, type TEXT, tag TEXT);
-CREATE TABLE proxy (type TEXT, config JSON, tag TEXT);
-CREATE TABLE route (type TEXT, config JSON, tag TEXT);
+CREATE TABLE IF NOT EXISTS host (name TEXT, addr4 TEXT, addr6 TEXT);
+CREATE TABLE IF NOT EXISTS user (name TEXT, token TEXT, config JSON);
+CREATE TABLE IF NOT EXISTS proxy (host TEXT, port INTEGER, type TEXT, config JSON, tag TEXT);
+CREATE TABLE IF NOT EXISTS access (user TEXT, class TEXT, tag TEXT);
+CREATE TABLE IF NOT EXISTS secret (name TEXT, value TEXT);
+
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__host__name ON host (name);
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__user__name ON user (name);
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__user__token ON user (token);
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__proxy__host__port__type ON proxy(host, port, type);
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__access__user__class__tag ON access (user, class, tag);
+CREATE UNIQUE INDEX IF NOT EXISTS constraint__secret__name ON secret (name);
