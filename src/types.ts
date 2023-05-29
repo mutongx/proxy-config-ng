da7072d33f0fe51e137d8a4dbec82fbf5b5da582
Mut Ze <mutongsoft@gmail.com>
2023-05-29 10:19:02 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,38 @@
+export interface Env {
+  SECRETS: KVNamespace;
+  DB: D1Database;
+  GITHUB_REPO: string;
+  GITHUB_REF: string;
+  GITHUB_TOKEN: string;
+};
+
 export type User = {
-  name: string,
-  token: string,
-  config: object | null,
+  name: string;
+  token: string;
+  config: object | null;
 };
 
 export type Host = {
-  name: string,
-  addr4: string,
-  addr6: string,
-}
+  name: string;
+  addr4: string;
+  addr6: string;
+};
+
+export type Proxy = {
+  host: string;
+  port: number;
+  type: string;
+  config: object | null;
+  tag: string;
+};
 
 export type Access = {
-  user: string,
-  class: string,
-  tag: string
+  user: string;
+  class: string;
+  tag: string;
 };
 
 export type Secret = {
-  name: string,
-  value: string,
-}
-
-export type Proxy = {
-  host: string,
-  port: number,
-  type: string,
-  config: object | null;
-  tag: string,
+  name: string;
+  value: string;
 };
