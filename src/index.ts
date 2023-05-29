69368627d22454522f97dc9dd9b8d90ad55ed622
Mut Ze <mutongsoft@gmail.com>
2023-05-29 13:11:16 +0800

    Use this.env; Move fillConfig to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 import { Env } from "./types";
 import Worker from "./worker"
-
+import * as pointer from "json-pointer";
 
 export default {
+
+  async fillConfig(worker: Worker, obj: any, args: object) {
+    if (obj === null) {
+      return;
+    }
+    if (typeof obj !== "object") {
+      return;
+    }
+    if (Array.isArray(obj)) {
+      return;
+    }
+    const keys = Object.keys(obj);
+    for (const key of keys) {
+      const value = obj[key];
+      if (value.$ref) {
+        const url = new URL(value.$ref);
+        if (url.protocol == "args:") {
+          var newValue;
+          try {
+            newValue = pointer.get(args, url.pathname);
+          } catch (e) {
+            newValue = url.searchParams.get("default");
+          }
+          obj[key] = newValue;
+        } else if (url.protocol == "secrets:") {
+          obj[key] = await worker.getSecret(url.pathname);
+        } else {
+          throw new Error(`unsupported ref: ${value.$ref}`);
+        }
+      } else {
+        await this.fillConfig(worker, value, args);
+      }
+    }
+  },
 
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
@@ -14,25 +48,25 @@ export default {
     const token = paths[1];
     const worker = new Worker(env);
 
-    const user = await worker.getUser(env, token);
+    const user = await worker.getUser(token);
     if (!user) {
       return new Response("not found", { status: 404 });
     }
 
-    const accesses = await worker.getAccesses(env, user);
-    const proxies = await worker.getProxies(env, accesses);
-    const proxyTypes = [... new Set(proxies.map((val) => val.type))]
-    const proxyConfigs = await worker.getProxiesConfig(env, proxyTypes);
+    const accesses = await worker.getAccesses(user);
+    const proxies = await worker.getProxies(accesses);
+    const proxyConfigs = await worker.getProxiesConfig([... new Set(proxies.map((val) => val.type))]);
 
-    var proxyResults = []
+    var outboundsConfig = []
     for (const proxy of proxies) {
       const config = structuredClone(proxyConfigs.get(proxy.type));
-      const addr = await worker.getHostAddr(env, proxy.host);
-      await worker.fillConfig(env, {
+      const addr = await worker.getHostAddr(proxy.host);
+      await this.fillConfig(worker, config, {
         "server": addr,
         "server_port": proxy.port,
-      }, config);
-      proxyResults.push(config);
+        ...(user.config ? user.config.args : null)
+      });
+      outboundsConfig.push(config);
     }
 
     const result = {
@@ -43,7 +77,7 @@ export default {
           "listen_port": 5353,
         }
       ],
-      "outbounds": proxyResults,
+      "outbounds": outboundsConfig,
     }
 
     return new Response(JSON.stringify(result, null, 2));
--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,5 @@
 import { Env } from "./types";
 import { Access, Host, Proxy, Secret, User } from "./types";
-import * as pointer from "json-pointer";
 
 export default class {
 
@@ -10,40 +9,40 @@ export default class {
     this.env = env;
   }
 
-  async getUser(env: Env, token: string): Promise<User | null> {
-    const stmt = env.DB.prepare("SELECT * FROM user WHERE token = ?1").bind(token);
+  async getUser(token: string): Promise<User | null> {
+    const stmt = this.env.DB.prepare("SELECT * FROM user WHERE token = ?1").bind(token);
     const user = await stmt.first() as User | null;
     return user;
   }
 
-  async getAccesses(env: Env, user: User): Promise<Array<Access>> {
-    const stmt = env.DB.prepare("SELECT * FROM access WHERE user = ?1").bind(user.name);
+  async getAccesses(user: User): Promise<Array<Access>> {
+    const stmt = this.env.DB.prepare("SELECT * FROM access WHERE user = ?1").bind(user.name);
     const result = await stmt.all();
     const accesses = result.results! as Array<Access>;
     return accesses;
   }
 
-  async getProxies(env: Env, access: Array<Access>): Promise<Array<Proxy>> {
+  async getProxies(access: Array<Access>): Promise<Array<Proxy>> {
     const tags = access.filter((val) => val.class == "proxy").map((val) => val.tag);
     if (!tags) {
       return [];
     }
-    const stmt = env.DB.prepare("SELECT * FROM proxy WHERE " + Array(tags.length).fill("tag = ?").join(" OR ")).bind(...tags);
+    const stmt = this.env.DB.prepare("SELECT * FROM proxy WHERE " + Array(tags.length).fill("tag = ?").join(" OR ")).bind(...tags);
     const result = await stmt.all();
     const proxies = result.results! as Array<Proxy>;
     return proxies;
   }
 
-  async getProxiesConfig(env: Env, proxyTypes: Array<string>): Promise<Map<string, object>> {
+  async getProxiesConfig(proxyTypes: Array<string>): Promise<Map<string, object>> {
     var result: Map<string, object> = new Map();
     for (const type of proxyTypes) {
       const resp = await fetch(
-        `https://api.github.com/repos/${env.GITHUB_REPO}/contents/proxies/${type}.json?ref=${env.GITHUB_REF}`,
+        `https://api.github.com/repos/${this.env.GITHUB_REPO}/contents/proxies/${type}.json?ref=${this.env.GITHUB_REF}`,
         {
           headers: {
             "Accept": "application/vnd.github.raw",
             "User-Agent": "Mutong's Cloudflare Workers",
-            "Authorization": `Bearer ${env.GITHUB_TOKEN}`
+            "Authorization": `Bearer ${this.env.GITHUB_TOKEN}`
           }
         });
       if (!resp.ok) {
@@ -54,8 +53,8 @@ export default class {
     return result;
   }
 
-  async getHostAddr(env: Env, name: string) {
-    const stmt = env.DB.prepare("SELECT * FROM host WHERE name = ?1").bind(name);
+  async getHostAddr(name: string) {
+    const stmt = this.env.DB.prepare("SELECT * FROM host WHERE name = ?1").bind(name);
     const host = await stmt.first() as Host | null;
     if (!host) {
       return null;
@@ -63,47 +62,13 @@ export default class {
     return host.addr4;
   }
 
-  async getSecret(env: Env, name: string) {
-    const stmt = env.DB.prepare("SELECT * FROM secret WHERE name = ?1").bind(name);
+  async getSecret(name: string) {
+    const stmt = this.env.DB.prepare("SELECT * FROM secret WHERE name = ?1").bind(name);
     const secret = await stmt.first() as Secret | null;
     if (!secret) {
       return null;
     }
     return secret.value;
-  }
-
-  async fillConfig(env: Env, args: object, obj: any) {
-    if (obj === null) {
-      return;
-    }
-    if (typeof obj !== "object") {
-      return;
-    }
-    if (Array.isArray(obj)) {
-      return;
-    }
-    const keys = Object.keys(obj);
-    for (const key of keys) {
-      const value = obj[key];
-      if (value.$ref) {
-        const url = new URL(value.$ref);
-        if (url.protocol == "args:") {
-          var newValue;
-          try {
-            newValue = pointer.get(args, url.pathname);
-          } catch (e) {
-            newValue = url.searchParams.get("default");
-          }
-          obj[key] = newValue;
-        } else if (url.protocol == "secrets:") {
-          obj[key] = await this.getSecret(env, url.pathname);
-        } else {
-          throw new Error(`unsupported ref: ${value.$ref}`);
-        }
-      } else {
-        await this.fillConfig(env, args, value);
-      }
-    }
   }
 
 }
